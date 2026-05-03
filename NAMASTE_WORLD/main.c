a24f023a87d72740e256a2267b3dfe5bded914e2
Amit Anand Jha <amitanandjha12@zohomail.in>
2026-05-03 23:27:03 +0530

    Add files via upload

--- NAMASTE_WORLD/main.c
+++ NAMASTE_WORLD/main.c
@@ -1,4 +1,4 @@
-/* =====================================================
+/* ====================================================================
  *
  * File Name   : main.c
  * File Type   : Source File (C)
@@ -6,16 +6,38 @@
  *
  * File Usage  : This is where the linker comes to pick the 'main'
  * function
- * ===================================================== */
+ * ==================================================================== */
 
+/* ====================================================================
+ *                             INCLUDES
+ * ==================================================================== */
 #include "main.h"
 
+
+/* ====================================================================
+ *                       FUNCTION DEFINITIONS
+ * ==================================================================== */
+
+
+/* ====================================================================
+ * Driver for ARIESV3.0 to print character given as input to this API
+ * Before printing we check if the board is ready to pick the new char
+ * to be printed or not - thats the while check. Check MACRO definitions
+ * ==================================================================== */
 void uart_putc(char c) {
     /* Wait for Transmit Holding Register to be empty */
     while (!(UART_LSR & UART_LSR_THRE));
     UART_THR = c;
 }
 
+
+
+/* ====================================================================
+ * Driver for ARIESV3.0 to print any string given as input to this API
+ * This API uses the uart_putc API internally. The entire string is basically
+ * parsed to each individual character and then printed unless we encounter
+ * a Null Terminator for strings - \0 is what strings end with
+ * ==================================================================== */
 void print_str(const char* s) {
     while (*s != '\0') {
         uart_putc(*s++);
@@ -26,11 +48,17 @@ void print_str(const char* s) {
 /* Defining the delay API which can basically help to insert delays */
 void delay(volatile int count) {
     while(count--) {
-
     	DO_NOTHING_CONSUME_A_CYCLE;
     }
 }
 
+
+
+
+
+/* ====================================================================
+ *                             MAIN
+ * ==================================================================== */
 
 int main() {
     // Keep it extremely simple: no extra functions, just the loop.
@@ -70,7 +98,7 @@ int main() {
         delay(10000);
         UART_THR = '\r';
 
-        print_str("\nNamaste World above was printed by writing each alphabet directly on the register address");
+        print_str("\nAbove was printed by writing each alphabet directly on the register address");
         print_str("\n\n");
     }
     return 0;
--- NAMASTE_WORLD/main.h
+++ NAMASTE_WORLD/main.h
@@ -14,6 +14,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
+/* ====================================================================
+ *                             MACROS
+ * ==================================================================== */
+
 /* The baud rate (bits per second) at which communication with ARIESV3.0 needs to happen */
 #define BAUD_RATE                     115200
 
@@ -27,7 +32,9 @@
 /* Defining Address to the LSR (LINE STATUS REGISTER) */
 #define UART_LSR                      (*(volatile unsigned int*)(UART0_BASE + 0x14))
 
-/* Transmit Holding Register Empty */
+/* Transmit Holding Register Empty - the 5th bit of LSR register
+ * IF UART_LSR_THRE = 0 => Transmit Register is still busy sending previous character
+ * IF UART_LSR_THRE = 1 => THR Register is empty and new character can be sent */
 #define UART_LSR_THRE                 0x20
 
 
@@ -35,8 +42,14 @@
 #define DO_NOTHING_CONSUME_A_CYCLE    __asm__ volatile ("nop")
 
 
+
+/* ====================================================================
+ *                      FUNCTUION DECLARATIONS
+ * ==================================================================== */
 /* Declaring the delay API which can basically help to insert delays */
 void delay(volatile int count);
+void uart_putc(char c);
+void print_str(const char* s);
 
 
 #endif /* MAIN_H_ */
